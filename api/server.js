4915ea134ecf56ee56e68b2d72703c73783d2014
DeveloperX <wsnyder2022@gmail.com>
2026-07-09 19:16:37 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://api.erlc.gg/v2/server?Players=true",
+      {
+        headers: {
+          "server-key": "uupNgrOZhpdCgwStivKZ-WiDezheWTtqIXLQIlLmoICWddCjlpumqMyjkJfaJ"
+        }
+      }
+    );
 
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+}
