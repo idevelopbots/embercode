50d23fd682fd78b3d3dd5f02951debfd862d845d
DeveloperX <wsnyder2022@gmail.com>
2026-07-09 19:17:31 -0400

    Update players.js

--- api/players.js
+++ api/players.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+    const response = await fetch(
+        "https://api.erlc.gg/v2/server?Players=true",
+        {
+            headers: {
+                "server-key": "uupNgrOZhpdCgwStivKZ-WiDezheWTtqIXLQIlLmoICWddCjlpumqMyjkJfaJ"
+            }
+        }
+    );
 
+    const data = await response.json();
+
+    res.status(200).json(data.Players || []);
+}
