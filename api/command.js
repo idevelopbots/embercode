dcaa8221d6d8a76144158288fe802cc658a373b8
DeveloperX <wsnyder2022@gmail.com>
2026-07-09 19:29:57 -0400

    Update command.js

--- api/command.js
+++ api/command.js
@@ -1,28 +1,39 @@
 export default async function handler(req, res) {
 
-    if(req.method !== "POST"){
+    if (req.method !== "POST") {
         return res.status(405).json({
             error: "Method not allowed"
         });
     }
 
-    const { command } = req.body;
+    try {
 
-    const response = await fetch(
-        "https://api.erlc.gg/v2/server/command",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "server-key": "uupNgrOZhpdCgwStivKZ-WiDezheWTtqIXLQIlLmoICWddCjlpumqMyjkJfaJ"
-            },
-            body: JSON.stringify({
-                command
-            })
-        }
-    );
+        const { command } = req.body;
 
-    const data = await response.json();
+        const response = await fetch(
+            "https://api.erlc.gg/v2/server/command",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "server-key": "uupNgrOZhpdCgwStivKZ-WiDezheWTtqIXLQIlLmoICWddCjlpumqMyjkJfaJ"
+                },
+                body: JSON.stringify({
+                    command: command
+                })
+            }
+        );
 
-    res.status(200).json(data);
+        const data = await response.json();
+
+        return res.status(200).json(data);
+
+    } catch (error) {
+
+        return res.status(500).json({
+            success: false,
+            error: error.message
+        });
+
+    }
 }
