1c49103eba06c5fd288bf069586c5bd57e6e7af1
DeveloperX <wsnyder2022@gmail.com>
2026-07-09 21:30:06 -0400

    Update staff.js

--- api/staff.js
+++ api/staff.js
@@ -1,1 +1,17 @@
+// api/staff.js
+export default async function handler(req, res) {
 
+    const response = await fetch(
+        "https://api.erlc.gg/v2/server?Staff=true",
+        {
+            headers: {
+                "server-key": "uupNgrOZhpdCgwStivKZ-WiDezheWTtqIXLQIlLmoICWddCjlpumqMyjkJfaJ"
+            }
+        }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+}
