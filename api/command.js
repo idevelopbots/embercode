2fcda791c9a1593c0fb095f2ebca95f09a3006aa
DeveloperX <wsnyder2022@gmail.com>
2026-07-09 20:07:29 -0400

    Update command.js

--- api/command.js
+++ api/command.js
@@ -8,6 +8,8 @@ export default async function handler(req, res) {
 
     try {
 
+        console.log("BODY:", req.body);
+
         const command = req.body.command;
 
         const response = await fetch(
@@ -26,12 +28,17 @@ export default async function handler(req, res) {
 
         const text = await response.text();
 
+        console.log("ERLC STATUS:", response.status);
+        console.log("ERLC RESPONSE:", text);
+
         return res.status(200).json({
             status: response.status,
             response: text
         });
 
     } catch (error) {
+
+        console.error(error);
 
         return res.status(500).json({
             error: error.message
