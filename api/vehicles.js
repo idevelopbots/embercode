4a1bee70dd6366e39e11758a11a5a54cd011128c
DeveloperX <wsnyder2022@gmail.com>
2026-07-09 20:52:57 -0400

    Create vehicles.js

---
+++ api/vehicles.js
@@ -0,0 +1,28 @@
+// api/vehicles.js
+
+export default async function handler(req, res) {
+
+    try {
+
+        const response = await fetch(
+            "https://api.erlc.gg/v2/server?Vehicles=true",
+            {
+                headers: {
+                    "server-key": "uupNgrOZhpdCgwStivKZ-WiDezheWTtqIXLQIlLmoICWddCjlpumqMyjkJfaJ"
+                }
+            }
+        );
+
+        const data = await response.json();
+
+        res.status(200).json(data.Vehicles || []);
+
+    } catch(error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
+
+}
