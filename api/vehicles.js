59e1323bf2f2990451ebe2c22c5673362607a797
DeveloperX <wsnyder2022@gmail.com>
2026-07-12 16:25:13 -0400

    Update vehicles.js

--- api/vehicles.js
+++ api/vehicles.js
@@ -1,1 +1,26 @@
+import { ERLC_KEY } from "../config.js";
 
+export default async function handler(req, res) {
+    try {
+
+        const response = await fetch(
+            "https://api.erlc.gg/v2/server?Vehicles=true",
+            {
+                headers: {
+                    "server-key": ERLC_KEY
+                }
+            }
+        );
+
+        const data = await response.json();
+
+        res.status(200).json(data.Vehicles || []);
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
+}
